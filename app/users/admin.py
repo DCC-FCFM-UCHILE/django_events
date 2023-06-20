84b9928d4f15315564712403ca7e648fcb430a84
Juan Arriagada <jarrcan@gmail.com>
2023-06-20 11:31:22 -0400

    ✨ sso guarda data extra si custom user está activo

--- app/users/admin.py
+++ app/users/admin.py
@@ -5,10 +5,14 @@ from django.utils.html import mark_safe
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from django_json_widget.widgets import JSONEditorWidget
 
 from .models import CustomUser
+
+
+from django.contrib.auth.admin import UserAdmin
 
 
 admin.site.unregister(Group)
@@ -28,13 +32,41 @@ class GroupAdmin(BaseGroupAdmin):
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = [
         "username",
         "alias",
         "email",
         "foto_img_tag",
     ]
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (_("Portal DCC"), {"fields": ("alias", "url_foto", "data")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
 
     def foto_img_tag(self, obj):
         return mark_safe(f'<img loading="lazy" src="{obj.url_foto}" alt="{str(obj.alias)}" height="30px" />')
--- app/users/models.py
+++ app/users/models.py
@@ -7,7 +7,6 @@ from django.db import models
 class CustomUser(AbstractUser):
     url_foto = models.URLField(max_length=250, null=True, blank=True)
     alias = models.CharField(max_length=250)
-
     data = models.JSONField(null=True, blank=True)
 
     def __str__(self):
