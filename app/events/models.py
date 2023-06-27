b3c1b88aebc288b27128afd8cd28badd2ded7f05
Juan Arriagada <jarrcan@gmail.com>
2023-06-27 16:46:14 -0400

    ⚡️ eventos ahora se disparan encadenados

--- app/events/models.py
+++ app/events/models.py
@@ -19,27 +19,3 @@ class Event(models.Model):
 
     def __str__(self):
         return f"{self.entity}:{self.key}:{self.action}" if self.key else f"{self.entity}:{self.action}"
-
-    @staticmethod
-    def emitt():
-        events = Event.objects.all()
-        if events:
-            client = _get_mqtt_client()
-
-            entities = {}
-            for e in events:
-                if e.entity not in entities:
-                    entities[e.entity] = {}
-                if e.action not in entities[e.entity]:
-                    entities[e.entity][e.action] = []
-                if e.key:
-                    entities[e.entity][e.action].append(e.key)
-
-            for entity, actions in entities.items():
-                for action, keys in actions.items():
-                    client.publish(
-                        settings.MQTT_TOPIC,
-                        f"{settings.MQTT_ORIGIN}:{entity}:{'|'.join(keys)}:{action}" if keys else f"{settings.MQTT_ORIGIN}:{entity}",
-                    )
-            client.disconnect()
-            events.delete()
