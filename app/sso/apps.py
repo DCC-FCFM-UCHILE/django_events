97b4acb95d3e6ffcc7c5ab6935e5f855897f526d
jarriagadac <jarrcan@gmail.com>
2024-03-20 18:03:48 -0300

    ♻️ refactor y limpieza

--- app/sso/apps.py
+++ app/sso/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class SsoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sso"
+
+    def ready(self):
+        from django.conf import settings
+        from core.functions import get_env_variable
+
+        settings.LOGIN_URL = "sso_index"
+        settings.SSO_URL = "https://portal.dcc.uchile.cl"
+        settings.SSO_APP = get_env_variable("DJANGO_SSO_APP")
+        settings.SSO_AUTH = get_env_variable("DJANGO_SSO_AUTH")
