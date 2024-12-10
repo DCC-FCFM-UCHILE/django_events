5d95338b3bf6b49db37f10c637a394b18d49f8bf
jarriagadac <jarrcan@gmail.com>
2024-12-10 14:25:45 +0000

    Eliminar métodos innecesarios en la configuración de la aplicación común

--- app/common/apps.py
+++ app/common/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.staticfiles.finders import AppDirectoriesFinder
-from django.contrib.staticfiles.storage import staticfiles_storage
 
 
 class CommonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "common"
-
-    def ready(self):
-
-        AppDirectoriesFinder.storages[self.name] = staticfiles_storage
